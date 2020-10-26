35651e4e914e9541cf3e8867a75dcb1a59205d91
erdose <erdos.attis@gmail.com>
2020-10-26 21:23:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# Xiaomi Mi Temperature and Humidity Monitor 2 - Domoticz
+
+[![Python](https://img.shields.io/badge/python-3-brightgreen.svg)]()![license](https://img.shields.io/github/license/erdose/xiaomi-mi-lywsd03mmc)![downloads](https://img.shields.io/github/downloads/erdose/xiaomi-mi-lywsd03mmc/total)![closed_issue](https://img.shields.io/github/issues-closed-raw/erdose/xiaomi-mi-lywsd03mmc)
+
+The Xiaomi Mi sensor provides temperature and humidity over BLE.
+
+![xiaomi_mi_2](Pictures/mi-temperature-and-humidity-monitor-2.jpg)
+
+## How it works
+
+1. ##### Preparing Domoticz
+
+   Create a virtual sensor (Temperature & Humidity) in Domoticz (as much as you need).
+
+   ![virtual_sensor](Pictures/temp_humid.jpg)
+
+   Note the IDX value of virtual sensor (Setup/Devices)!
+
+   ![virtual_sensor_idx](Pictures/temp_idx.jpg)
+
+   If you want to get the raw voltage of battery, you also need to create the Voltage sensors too. The IDX value of the sensor is also required!
+
+2. ##### Finding the Bluetooth MAC Address of the sensor
+
+   Turn on the Xiaomi Mi sensor (Insert the battery).
+
+   Run the following command to find the MAC address:
+
+   ```shell
+   sudo hcitool lescan
+   ```
+
+   Example result:
+
+   ```shell
+   LE Scan ...
+   46:4D:55:28:41:CA (unknown)
+   A4:C1:38:DC:8F:2E LYWSD03MMC
+   ```
+
+   The name is always LYWSD03MMC.
+
+   Note down the MAC address!
+
+3. ##### Edit the xiaomiBleLywsd03mmc.py script
+
+   Clone repository:
+
+   ```shell
+   git clone https://github.com/erdose/xiaomi-mi-lywsd03mmc.git
+   ```
+
+   Open the ***xiaomiBleLywsd03mmc.py*** and edit the parameters at the top of the script!
+
+   ```shell
+   cd xiaomi-mi-lywsd03mmc
+   sudo nano xiaomiBleLywsd03mmc.py
+   ```
+
+   ```python
+   # domoticz configuration
+   DOMOTICZ_SERVER_IP = "xxx.xxx.x.xxx"
+   DOMOTICZ_SERVER_PORT = "xxxx"
+   DOMOTICZ_USERNAME = ""
+   DOMOTICZ_PASSWORD = ""
+   ```
+
+   *MAC* : MAC address of the Xiaomi Mi sensor.
+
+   *TH_IDX* : IDX value of the Temperature & Humidity sensor(s) in Domoticz.
+
+   *VOLTAGE_IDX* : IDX value of the Voltage sensor(s) in Domoticz.
+
+   ```python
+   # sensor dictionary to add own sensors
+   # if you don't want to use the raw voltage option, just write -1 in the VOLTAGE_IDX value field
+   sensors = { 1: {"MAC": "xx:xx:xx:xx:xx:xx", "TH_IDX": 1, "VOLTAGE_IDX": -1},
+   			2: {"MAC": "xx:xx:xx:xx:xx:xx", "TH_IDX": 2, "VOLTAGE_IDX": -1},
+   			3: {"MAC": "xx:xx:xx:xx:xx:xx", "TH_IDX": 3, "VOLTAGE_IDX": -1}}
+   ```
+
+4. ##### Schedule the update interval
+
+   Enable the script to run at a regular interval (5 mins):
+
+   ```shell
+   sudo crontab-e
+   ```
+
+   Add this line:
+
+   ```shell
+   */5 * * * * python3 /home/pi/xiaomi-mi-lywsd03mmc/xiaomiBleLywsd03mmc.py
+   ```
+
+   Done! 
---
+++ xiaomiBleLywsd03mmc.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import binascii
+import base64
+import requests
+from bluepy.btle import UUID, Peripheral, ADDR_TYPE_PUBLIC, DefaultDelegate, Scanner
+
+# domoticz configuration
+DOMOTICZ_SERVER_IP = "xxx.xxx.x.xxx"
+DOMOTICZ_SERVER_PORT = "xxxx"
+DOMOTICZ_USERNAME = ""
+DOMOTICZ_PASSWORD = ""
+
+# sensor dictionary to add own sensors
+# if you don't want to use the raw voltage option, just write -1 in the VOLTAGE_IDX value field
+sensors = { 1: {"MAC": "xx:xx:xx:xx:xx:xx", "TH_IDX": 1, "VOLTAGE_IDX": -1},
+			2: {"MAC": "xx:xx:xx:xx:xx:xx", "TH_IDX": 2, "VOLTAGE_IDX": -1},
+			3: {"MAC": "xx:xx:xx:xx:xx:xx", "TH_IDX": 3, "VOLTAGE_IDX": -1}}
+
+battery_level = -1
+sensor_id = -1
+
+class TempHumDelegate(DefaultDelegate):
+	def __init__(self):
+		DefaultDelegate.__init__(self)
+
+	def handleNotification(self, cHandle, data):
+		temperature = int.from_bytes(data[0:2],byteorder='little',signed=True)/100
+		print("Temp:",temperature)
+		humidity = int.from_bytes(data[2:3],byteorder='little')
+		print("Hum:",humidity)
+		voltage=int.from_bytes(data[3:5],byteorder='little') / 1000.
+		print("Voltage:",voltage)
+		batteryLevel = min(int(round((voltage - 2.1),2) * 100), 100)        #3.1 or above --> 100% 2.1 --> 0 %
+		print("Battery level:",batteryLevel)
+		comfort_type = get_comfort_type(humidity)
+		if (sensor_id != -1 and batteryLevel > -1):
+			for number, sensor in sensors.items():
+				if sensor["TH_IDX"] == sensor_id:
+					request_url = 'http://' + DOMOTICZ_SERVER_IP + ':' + DOMOTICZ_SERVER_PORT + '/json.htm?type=command&param=udevice&idx=' + str(sensor_id) + '&nvalue=0&svalue=' + str(temperature) + ';' + str(humidity) + ';' + comfort_type + '&battery=' + str(batteryLevel)
+					send_to_domoticz(request_url)
+					if sensor["VOLTAGE_IDX"] != -1:
+						request_url = 'http://' + DOMOTICZ_SERVER_IP + ':' + DOMOTICZ_SERVER_PORT + '/json.htm?type=command&param=udevice&idx=' + str(sensor["VOLTAGE_IDX"]) + '&nvalue=0&svalue=' + str(voltage)
+						send_to_domoticz(request_url)
+
+def send_to_domoticz(url):
+	print(url)
+	requests.get(url, auth=(DOMOTICZ_USERNAME, DOMOTICZ_PASSWORD))
+
+def get_comfort_type(humidity):
+	comfort_type = "0"
+	if float(humidity) < 40:
+		comfort_type = "2"
+	elif float(humidity) <= 70:
+		comfort_type = "1"
+	elif float(humidity) > 70:
+		comfort_type = "3"
+	return comfort_type
+
+def handle_temp_hum_value():
+	while True:
+		if p.waitForNotifications(10.0):
+			break
+
+for number, sensor in sensors.items():
+	try:
+		sensor_id = sensor["TH_IDX"]
+		print("TH_IDX:%d"%(sensor["TH_IDX"]))
+		p = Peripheral(sensor["MAC"])
+		p.writeCharacteristic(0x0038, b'\x01\x00', True)      #enable notifications of Temperature, Humidity and Battery voltage
+		p.writeCharacteristic(0x0046, b'\xf4\x01\x00', True)
+		p.withDelegate(TempHumDelegate())
+		handle_temp_hum_value()
+		p.disconnect()
+	except Exception as e:
+		print(str(e))
+		pass
