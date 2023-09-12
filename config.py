3531eec5f2a9bf967f822495ddaa5273bb0f509b
famascl3m <40993037+famascl3m@users.noreply.github.com>
2023-09-12 15:48:57 +0200

    Update config.py
    remove login and password to use base64 encoded login:password to work with the latest domoticz 2023.2

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # domoticz configuration
 DOMOTICZ_SERVER_IP = "xxx.xxx.x.xxx"
 DOMOTICZ_SERVER_PORT = "xxxx"
-DOMOTICZ_USERNAME = ""
-DOMOTICZ_PASSWORD = ""
+# username:password base64 encoded since Domoticz version 2023.1 changed API authentication
+DOMOTICZ_CREDENTIALS = "xxxxxxxx"
 
 # sensor dictionary to add own sensors
 # if you don't want to use the raw voltage option, just write -1 in the VOLTAGE_IDX value field
